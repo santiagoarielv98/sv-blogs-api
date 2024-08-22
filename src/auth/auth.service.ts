087a1da9cef241a0d141fef0c9ce4bd27d321f7a
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-22 17:48:20 -0300

    feat: Update JWT token expiration to 50 days and add request logging middleware

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,20 +11,6 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
-    const user = await this.usersService.findOne(username);
-    const isMatch = await bcrypt.compare(pass, user.password);
-    if (!isMatch) {
-      throw new UnauthorizedException();
-    }
-    const payload = { sub: user.id, username: user.username };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
-  }
   async signUp(signUpDto: CreateUserDto) {
     const newUser = await this.usersService.create(signUpDto);
     const payload = { sub: newUser.id, username: newUser.username };
@@ -35,11 +21,14 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && (await bcrypt.compare(pass, user.password))) {
-      const { password: _pass, ...result } = user;
-      return result;
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException();
     }
-    return null;
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 
   async login(user: any) {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { useRequestLogging } from './middlewares/request-logging';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  useRequestLogging(app);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,7 +13,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+  await app.listen(8080);
 }
 bootstrap();
